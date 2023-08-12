17bbca7b75c93ccee78535f75cf671cbb9927a74
Santiago Gutierrez <santiagogutierrezcode@gmail.com>
2023-08-12 10:49:47 -0300

    nuevo commit

--- src/index.js
+++ src/index.js
@@ -21,17 +21,17 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig)
   export const db = getFirestore(app)
 
-  //const productIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  const productIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-  //productIds.forEach((productId) => {
-    //getProductsById(productId)
-      //.then((product) => {
-        // Aquí puedes agregar el producto a la base de datos
-      //})
-      //.catch((error) => {
-        //console.error('Error fetching product by ID:', error);
-      //});
-  //});
+  productIds.forEach((productId) => {
+    getProductsById(productId)
+      .then((product) => {
+        db.collection("products").add(product)
+      })
+      .catch((error) => {
+        console.error('Error fetching product by ID:', error);
+      });
+  });
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
